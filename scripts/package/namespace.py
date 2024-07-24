2127a509e2134f6406167fc74daf1e5da3a5dbc3
Serious-senpai <57554044+Serious-senpai@users.noreply.github.com>
2024-07-24 22:38:22 +0700

    Set 500 iterations as default

--- scripts/package/namespace.py
+++ scripts/package/namespace.py
@@ -23,7 +23,7 @@ parser = argparse.ArgumentParser(
     formatter_class=argparse.ArgumentDefaultsHelpFormatter,
 )
 parser.add_argument("problem", type=str, help="the problem name in the archive")
-parser.add_argument("-i", "--iterations", default=100, type=int, help="the number of iterations to run the algorithm for")
+parser.add_argument("-i", "--iterations", default=500, type=int, help="the number of iterations to run the algorithm for")
 parser.add_argument("-t", "--tabu-size", default=10, type=int, help="the tabu size for each neighborhood")
 parser.add_argument("-c", "--config", default="linear", choices=["linear", "non-linear", "endurance"], help="the energy consumption model to use")
 parser.add_argument("--speed-type", default="low", choices=["low", "high"], help="speed type of drones")
