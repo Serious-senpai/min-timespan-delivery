9cf0fbbe6f9f3a4a71b89a8d5551d64d8ffd73bd
Serious-senpai <57554044+Serious-senpai@users.noreply.github.com>
2024-08-10 12:52:45 +0700

    Fix workflow

--- scripts/build.bat
+++ scripts/build.bat
@@ -5,16 +5,37 @@ g++ --version
 
 for %%f in ("%~dp0..") do set root=%%~ff
 echo Got root of repository: %root%
-
-set params=-Wall -I src/include -std=c++20
-if "%1"=="debug" (
-    set params=%params% -g -D DEBUG
-    echo Building in debug mode
-) else (
-    set params=%params% -O3
-)
-
 if not exist %root%\build mkdir %root%\build
-echo Compiling %root%/src/main.cpp to %root%/build/main.exe
-echo Running "g++ %params% %root%/src/main.cpp -o %root%/build/main.exe"
-g++ %params% %root%/src/main.cpp -o %root%/build/main.exe
+
+if "%1"=="alglib" (
+    echo Building ALGLIB
+    for %%f in (%root%\extern\alglib-cpp\src\*.cpp) do (
+        set command=g++ -c -O3 -D AE_CPU=AE_INTEL -m avx2 -m fma -std=c++20 %%f -o %root%/build/%%~nf.o
+        echo Running "!command!"
+        !command!
+    )
+
+) else (
+    echo Building main.exe
+    set params=-Wall -I src/include -I extern/alglib-cpp/src -std=c++20
+    if "%1"=="debug" (
+        set params=!params! -g -D DEBUG
+        echo Building in debug mode
+
+    ) else (
+        set params=!params! -O3
+        if "%1"=="profile-generate" (
+            set params=!params! -f profile-generate
+            echo Building in profile mode
+
+        ) else if "%1"=="profile-use" (
+            set params=!params! -f profile-use
+            echo Building using generated profile data
+
+        ) else echo Building normally
+    )
+
+    set command=g++ !params! %root%/src/main.cpp %root%/build/*.o -o %root%/build/main.exe
+    echo Running "!command!"
+    !command!
+)
--- scripts/build.sh
+++ scripts/build.sh
@@ -9,13 +9,12 @@ mkdir -p ROOT_DIR/build
 
 if [ "$1" == "alglib" ]
 then
-    echo "Building alglib"
+    echo "Building ALGLIB"
 
     #! https://stackoverflow.com/a/9612560
     find $ROOT_DIR/extern/alglib-cpp/src/*.cpp -print0 | while read -d $'\0' file
     do
-        name=$(basename ${file%.cpp})
-        command="g++ -c -O3 -D AE_CPU=AE_INTEL -mavx2 -mfma -std=c++20 $file -o $ROOT_DIR/build/$name.o"
+        command="g++ -c -O3 -D AE_CPU=AE_INTEL -m avx2 -m fma -std=c++20 $file -o $ROOT_DIR/build/$(basename ${file%.cpp}).o"
         echo "Running \"$command\""
         $command
     done
@@ -32,12 +31,12 @@ else
         params="$params -O3"
         if [ "$1" == "profile-generate" ]
         then
-            params="$params -fprofile-generate"
+            params="$params -f profile-generate"
             echo "Building in profile mode"
 
         elif [ "$1" == "profile-use" ]
         then
-            params="$params -fprofile-use"
+            params="$params -f profile-use"
             echo "Building using generated profile data"
 
         else
