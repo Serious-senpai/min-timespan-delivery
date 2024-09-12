7a8fade08040ca22213dbd9ef6d2529b8a5ad91c
Serious-senpai <57554044+Serious-senpai@users.noreply.github.com>
2024-09-12 16:06:44 +0700

    Add setup script when cloning repository

---
+++ scripts/setup.sh
@@ -0,0 +1,8 @@
+SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+ROOT_DIR=$(realpath $SCRIPT_DIR/..)
+
+mkdir $ROOT_DIR/.vscode
+cp $ROOT_DIR/c_cpp_properties.json $ROOT_DIR/.vscode/c_cpp_properties.json
+
+python3 -m venv $ROOT_DIR/.venv
+$ROOT_DIR/.venv/bin/pip install -r $ROOT_DIR/requirements.txt
