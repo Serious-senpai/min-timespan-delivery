75274a576fe477f5934637c178e63ec5b627a713
Serious-senpai <57554044+Serious-senpai@users.noreply.github.com>
2025-02-23 10:13:37 +0700

    Add missing tabu size factor

--- scripts/summary.py
+++ scripts/summary.py
@@ -1,7 +1,10 @@
 from __future__ import annotations
 
+import itertools
 import json
 import re
+import sqlite3
+from string import ascii_lowercase
 from typing_extensions import Dict, Iterable, Optional
 
 from package import MILPResultJSON, Problem, ResultJSON, SolutionJSON, ROOT, csv_wrap
@@ -29,10 +32,21 @@ def result_reader() -> Iterable[ResultJSON[SolutionJSON]]:
             yield data
 
 
+COLUMN_CHARS = set(ascii_lowercase + "_")
+
+
+def header_to_column(header: str) -> str:
+    header = header.lower().replace(" ", "_")
+    header = "".join(c for c in header if c in COLUMN_CHARS)
+    return header
+
+
 if __name__ == "__main__":
     milp = compare()
 
-    with ROOT.joinpath("result", "summary.csv").open("w") as csv:
+    path = ROOT / "result" / "summary.csv"
+    print(f"Writing to {path}")
+    with path.open("w") as csv:
         csv.write("sep=,\n")
         headers = [
             "Problem",
@@ -148,3 +162,127 @@ if __name__ == "__main__":
                 result["strategy"],
             ]
             csv.write(",".join(segments) + "\n")
+
+    path = ROOT / "result" / "summary.db"
+    print(f"Writing to {path}")
+
+    path.unlink(missing_ok=True)
+    with sqlite3.connect(path) as connection:
+        cursor = connection.cursor()
+
+        columns = [
+            "problem TEXT NOT NULL",
+            "customers_count INTEGER NOT NULL",
+            "trucks_count INTEGER NOT NULL",
+            "drones_count INTEGER NOT NULL",
+            "iterations INTEGER NOT NULL",
+            "tabu_size_factor INTEGER NOT NULL",
+            "reset_after_factor INTEGER NOT NULL",
+            "tabu_size INTEGER NOT NULL",
+            "reset_after INTEGER NOT NULL",
+            "max_elite_set_size INTEGER NOT NULL",
+            "destroy_rate INTEGER NOT NULL",
+            "energy_model TEXT NOT NULL",
+            "speed_type TEXT NOT NULL",
+            "range_type TEXT NOT NULL",
+            "waiting_time_limit INTEGER NOT NULL",
+            "truck_maximum_speed REAL",
+            "endurance_fixed_time REAL",
+            "endurance_drone_speed REAL",
+            "cost REAL NOT NULL",
+            "milp_cost REAL",
+            "milp_performance REAL",
+            "milp_status TEXT",
+            "capacity_violation REAL NOT NULL",
+            "energy_violation REAL NOT NULL",
+            "waiting_time_violation REAL NOT NULL",
+            "fixed_time_violation REAL NOT NULL",
+            "truck_paths TEXT NOT NULL",
+            "drone_paths TEXT NOT NULL",
+            "truck_working_time TEXT NOT NULL",
+            "drone_working_time TEXT NOT NULL",
+            "feasible INTEGER NOT NULL",
+            "initialization TEXT NOT NULL",
+            "last_improved INTEGER NOT NULL",
+            "elapsed REAL NOT NULL",
+            "url TEXT",
+            "weight_per_truck_route REAL NOT NULL",
+            "customers_per_truck_route REAL NOT NULL",
+            "truck_route_count INTEGER NOT NULL",
+            "weight_per_drone_route REAL NOT NULL",
+            "customers_per_drone_route REAL NOT NULL",
+            "drone_route_count INTEGER NOT NULL",
+            "strategy TEXT NOT NULL",
+        ]
+
+        query = "CREATE TABLE summary(" + ", ".join(columns) + ")"
+        print(query)
+        cursor.execute(query)
+
+        query = "INSERT INTO summary VALUES (" + ", ".join(itertools.repeat("?", len(columns))) + ")"
+        for result in result_reader():
+            milp_available = result["problem"] in milp
+            milp_feasible = milp_available and milp[result["problem"]]["status"] != "INFEASIBLE"
+
+            milp_time: Optional[float] = None
+            if milp_feasible:
+                milp_time = milp[result["problem"]]["Solve_Time"]  # type: ignore
+            elif milp_available:
+                milp_time = 36000.0  # 10 hours without any feasible solutions
+
+            truck_route_count = sum(len(routes) for routes in result["solution"]["truck_paths"])
+            drone_route_count = sum(len(routes) for routes in result["solution"]["drone_paths"])
+
+            problem = Problem.import_data(result["problem"])
+            truck_weight = sum(sum(sum(problem.demands[c] for c in route) for route in routes) for routes in result["solution"]["truck_paths"])
+            drone_weight = sum(sum(sum(problem.demands[c] for c in route) for route in routes) for routes in result["solution"]["drone_paths"])
+            truck_customers = sum(sum(len(route) - 2 for route in routes) for routes in result["solution"]["truck_paths"])
+            drone_customers = sum(sum(len(route) - 2 for route in routes) for routes in result["solution"]["drone_paths"])
+
+            cursor.execute(
+                query,
+                (
+                    result["problem"],
+                    int(result["problem"].split(".")[0]),
+                    result["trucks_count"],
+                    result["drones_count"],
+                    result["iterations"],
+                    result["tabu_size_factor"],
+                    result["reset_after_factor"],
+                    result["tabu_size"],
+                    result["reset_after"],
+                    result["max_elite_size"],
+                    result["destroy_rate"],
+                    result["config"],
+                    result["speed_type"],
+                    result["range_type"],
+                    result["waiting_time_limit"],
+                    result["truck_maximum_speed"],
+                    result["endurance_fixed_time"],
+                    result["endurance_drone_speed"],
+                    result["solution"]["cost"] / 60,
+                    milp[result["problem"]]["Optimal"] if milp_feasible else None,  # type: ignore
+                    milp_time,
+                    str(milp[result["problem"]]["status"]) if milp_available else "",
+                    result["solution"]["capacity_violation"],
+                    result["solution"]["drone_energy_violation"],
+                    result["solution"]["waiting_time_violation"],
+                    result["solution"]["fixed_time_violation"],
+                    str(result["solution"]["truck_paths"]),
+                    str(result["solution"]["drone_paths"]),
+                    str(result["solution"]["truck_working_time"]),
+                    str(result["solution"]["drone_working_time"]),
+                    int(result["solution"]["feasible"]),
+                    result["initialization_label"],
+                    result["last_improved"],
+                    result["elapsed"],
+                    result["url"],
+                    truck_weight / truck_route_count if truck_route_count > 0 else 0,
+                    truck_customers / truck_route_count if truck_route_count > 0 else 0,
+                    truck_route_count,
+                    drone_weight / drone_route_count if drone_route_count > 0 else 0,
+                    drone_customers / drone_route_count if drone_route_count > 0 else 0,
+                    drone_route_count,
+                    result["strategy"],
+                )
+            )
